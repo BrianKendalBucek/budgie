1991e9d622535d341c30a0906de3900e861e799a
Gord L <rndedg@gmail.com>
2023-01-24 09:10:23 -0800

    Changed colour of title header and also added package-lock.json for Sass work

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src='https://github.com/BrianKendalBucek/budgie/blob/main/assets/budgie-icon.png?raw=true' className="budgie-logo" alt="Budgie logo" />
+        <div class ="welcome-title">
+          <h1>Budgie</h1>
+          <p>Let's fly!</p>          
+        </div>
       </header>
     </div>
   );
