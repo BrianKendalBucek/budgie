f66166fb82390224707617f8aa83172acd05fdc2
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-01-24 09:16:22 -0600

    Got rid of junk code from tutorial setup and add .env

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,19 +1,19 @@
-import React, {useState, useEffect} from "react";
+// import React, {useState, useEffect} from "react";
 
-function Users() {
-  const [users, setUsers] = useState([])
+// function Users() {
+//   const [users, setUsers] = useState([])
 
-  useEffect(() => {
-    fetch("/users/").then(res => {
-      if(res.ok) {
-        return res.json()
-      }
-    }).then(jsonRes => setUsers(jsonRes.usersList))
-  })
+//   useEffect(() => {
+//     fetch("/users/").then(res => {
+//       if(res.ok) {
+//         return res.json()
+//       }
+//     }).then(jsonRes => setUsers(jsonRes.usersList))
+//   })
 
-  return (<div>
-    {users.map(user => <li>{user}</li>)}
-  </div>)
-}
+//   return (<div>
+//     {users.map(user => <li>{user}</li>)}
+//   </div>)
+// }
 
-export default Users;
+// export default Users;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import Users from "./components/Users";
+// import Users from "./components/Users";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div><App /><Users /></div>
+    <div><App /></div>
   </React.StrictMode>
 );
 
