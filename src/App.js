de1d2ccafc8d24cfc439c18d0066dc83d125d677
Gord L <rndedg@gmail.com>
2023-01-26 09:51:42 -0800

    Add PieChart progress and App for testing charts

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import './App.scss';
+import { Pie } from './components/PieChart/PieChart';
+import * as d3 from "d3";
 
+import React, { useState, useEffect } from "react";
 
 function App() {
+
+  /* generateData used to create random data for chart build testing purposes */
+  const generateData = (value, length = 5) =>
+  d3.range(length).map((item, index) => ({
+    date: index,
+    value: value === null || value === undefined ? Math.random() * 100 : value
+  }));
+
+  
+// const [data, setData] = useState(generateData());
+const changeData = () => {
+  setData(generateData());
+};
+
+// Hardcoded data in useState to eventually use data from API call/DB
+const [data, setData] = useState([{name: "CAD", value: 43}, {name: "USD", value: 412}, {name: "EUR", value: 665}, {name: "GBP", value: 123}]);
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,9 +30,18 @@ function App() {
         <img src='https://github.com/BrianKendalBucek/budgie/blob/main/assets/budgie-icon.png?raw=true' className="budgie-logo" alt="Budgie logo" />
         <div class ="welcome-title">
           <h1>Budgie</h1>
-          <p>Let's fly!</p>          
+          <p>Let's fly!</p>    
+          
         </div>
       </header>
+      {/* Pie just used to test chart display. TO BE REMOVED */}
+        <Pie
+          data={data}
+          width={200}
+          height={200}
+          innerRadius={70}
+          outerRadius={100}
+        />
     </div>
   );
 }
