d23a281a48f358008e6f444b673a0c166b54b1a5
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-01-25 20:36:16 -0600

    Progress on Header.js, example code

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,34 @@
 import React from "react";
+// import NavLink from 'react-router-dom'
+// import Logo from "assets/budgie-icon.png";
 
-export default function Header({time}) {
-  // return (<header className="appointment__time">
-  //   <h4 className="text--semi-bold">{time}</h4>
-  //   <hr className="appointment__separator" />
-  // </header>
-  // );
+const Header = () => {
+  // return(
+  //   <nav>
+  //     <div className="div-header">
+  //       <div className="div-svg">
+  //         {/* Logo */}
+  //       </div>
+  //       <div>
+  //         <NavLink to='/'><Home className="div-svg"/></NavLink>
+  //         <NavLink to='/explore'><Explore className="div-svg"/></NavLink>
+  //         <button className="button-header">Log Out</button>
+  //       </div>
+  //     </div>
+  //   </nav>
+  // )
 }
+
+export default Header;
+
+// BELOW IS EXAMPLE CODE TO CONNECT HEADER TO EVERY PAGE
+
+// const Home = () => {
+//   return(
+//     <div>
+//       <Header/>
+//     </div>
+//   )
+// }
+
+// export default Home;
