17c849442ff4bd235c9a2a1ef3ccf1b4a33c733a
Bryce Haley <bhaley91@gmail.com>
2023-01-26 18:05:56 -0500

    demo pulling categories

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.scss';
 import { Pie } from './components/PieChart/PieChart';
 import CategoryList from './components/CategoryList/CategoryList';
+import useApplicationData from "./hooks/useApplicationData.js";
 import * as d3 from "d3";
+
 
 import React, { useState } from "react";
 
 function App() {
+
+  const {
+    state,
+    setState
+  } = useApplicationData();
 
   /* generateData used to create random data for chart build testing purposes */
   const generateData = (value, length = 5) =>
@@ -22,7 +29,6 @@ const changeData = () => {
 
 // Hardcoded data in useState to eventually use data from API call/DB
 const [data, setData] = useState([{name: "CAD", value: 43}, {name: "USD", value: 412}, {name: "EUR", value: 665}, {name: "GBP", value: 123}]);
-const categoryData = [{id:1, name:"groceries"}];
 
   return (
     <div className="App">
@@ -44,10 +50,11 @@ const categoryData = [{id:1, name:"groceries"}];
           outerRadius={100}
         />
         <CategoryList 
-          categories = {categoryData}
+          categories = {state.categories}
         />
     </div>
   );
 }
+
 
 export default App;
