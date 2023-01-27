f06f0a6e6eb2f0ee21952cda17126c74f072a285
Gord L <rndedg@gmail.com>
2023-01-27 11:58:57 -0800

    Moved Welcome page layout from App.js to Welcome to begin implmenting React Router

--- src/App.js
+++ src/App.js
@@ -34,14 +34,6 @@ const [data, setData] = useState([{name: "CAD", value: 43}, {name: "USD", value:
           
         </div>
       </header>
-      {/* Pie just used to test chart display. TO BE REMOVED */}
-        <Pie
-          data={data}
-          width={200}
-          height={200}
-          innerRadius={70}
-          outerRadius={100}
-        />
     </div>
   );
 }
---
+++ src/components/Welcome/Welcome.js
@@ -0,0 +1,41 @@
+import './Welcome.scss';
+import { Pie } from './components/PieChart/PieChart';
+import * as d3 from "d3";
+
+import React, { useState, useEffect } from "react";
+
+function Welcome() {
+
+  /* generateData used to create random data for chart build testing purposes */
+  const generateData = (value, length = 5) =>
+  d3.range(length).map((item, index) => ({
+    date: index,
+    value: value === null || value === undefined ? Math.random() * 100 : value
+  }));
+
+  
+// const [data, setData] = useState(generateData());
+const changeData = () => {
+  setData(generateData());
+};
+
+// Hardcoded data in useState to eventually use data from API call/DB
+const [data, setData] = useState([{name: "CAD", value: 43}, {name: "USD", value: 412}, {name: "EUR", value: 665}, {name: "GBP", value: 123}]);
+
+
+  return (
+    <div className="Welcome">
+      <header className="Welcome-header">
+
+        <img src='https://github.com/BrianKendalBucek/budgie/blob/main/assets/budgie-icon.png?raw=true' className="budgie-logo" alt="Budgie logo" />
+        <div class ="welcome-title">
+          <h1>Budgie</h1>
+          <p>Let's fly!</p>    
+          
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Welcome;
