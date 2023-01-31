b84d27457fb91f12696b54c01bf747f603f87cd5
Gord L <rndedg@gmail.com>
2023-01-31 11:12:00 -0800

    Fixed a few errors on main

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -14,10 +14,10 @@ export function Statistics (props){
     { completed: 75 }
   ];
 
-export function Statistics() {
-
 
   const [data, setData] = useState([{ name: "CAD", value: 43 }, { name: "USD", value: 412 }, { name: "EUR", value: 665 }, { name: "GBP", value: 123 }])
+
+  
   return (
 
     <div className="stats-main">
@@ -30,12 +30,6 @@ export function Statistics() {
           <ProgressBar completed={item.completed} />
         ))}
 
-      <div className="stats-btns">
-        <Link to="/category">
-          <h1>Category</h1>
-        </Link>
-
-      </div>
       
       <div className="stats-btns">
         <h2>Links will be buttons in the future</h2>
@@ -46,7 +40,7 @@ export function Statistics() {
           <Link to="/converter"><li>Converter</li></Link>
         </ul>
 
-
+        </div>
       </div>
     </div>
   )
