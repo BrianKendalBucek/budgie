5e045157cb3db363a2fa3ba441a415b7cf794f53
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-01-30 18:56:13 -0600

    Added basic Converter structure

---
+++ src/components/Converter/Converter.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './Converter.scss';
+
+export function Converter() {
+
+  return (
+    <>
+      {/* <div className="primary-amount"> */}
+      {/* show input amount centered */}
+      {/* </div> */}
+      <div className="primary-currency">
+        {/* show input currency */}
+      </div>
+      <div>
+        <div className="secondary-currency">
+          {/* show secondary currency name left */}
+        </div>
+        <div className="secondary-amount">
+          {/* show secondary currency amount right */}
+        </div>
+      </div>
+      <div className="primary-amount-input">
+        <form action="?">
+          <label for="amount">Amount:</label>
+          <input type="integer" id="amount" name="amount"></input>
+          <input type="submit" value="Submit"></input>
+        </form>
+      </div>
+    </>
+  )
+
+}
