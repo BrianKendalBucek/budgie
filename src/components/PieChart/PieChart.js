73fdaae7118648a29b73a5a6d114609693976a53
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-01-31 13:33:40 -0600

    fixing bargraph merge problems

--- src/components/PieChart/PieChart.js
+++ src/components/PieChart/PieChart.js
@@ -64,58 +64,115 @@ export const Pie = props => {
     />
 */
 
-export const Bar = props => {
-  // const [data] = useState([200, 250, 60, 150, 100, 175]);
+export const Bar = () => {
   const svgRef = useRef();
-  const { data, width, height } = props;
+  // set the dimensions and margins of the graph
+  const margin = { top: 20, right: 30, bottom: 40, left: 90 },
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
+  // append the svg object to the body of the page
+  const svg = d3.select(svgRef.current)
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-  useEffect(() => {
-    // setting up svg container
-    // const w = 400;
-    // const h = 300;
-    const svg = d3.select(svgRef.current)
-      .attr('width', width)
-      .attr('height', height)
-      .style('overflow', 'visibile')
-      .style('margin-top', '75px');
+  // Parse the Data
+  d3.csv("https://raw.githubusercontent.com/holtzy/data_to_viz/master/Example_dataset/7_OneCatOneNum_header.csv").then(function (data) {
 
-    // setting the scaling
-    const xScale = d3.scaleBand()
-      .domain(data.map((val, i) => i))
-      .range([0, width])
-      .padding(0.5);
-    const yScale = d3.scaleLinear()
-      .domain([0, height])
-      .range([height, 0]);
+    // Add X axis
+    const x = d3.scaleLinear()
+      .domain([0, 13000])
+      .range([0, width]);
+    svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .call(d3.axisBottom(x))
+      .selectAll("text")
+      .attr("transform", "translate(-10,0)rotate(-45)")
+      .style("text-anchor", "end");
 
-    // setting the axis
-    const xAxis = d3.axisBottom(xScale)
-      .ticks(data.length);
-    const yAxis = d3.axisLeft(yScale)
-      .ticks(5);
-    svg.append('g')
-      .call(xAxis)
-      .attr('transform', `translate(0, ${height})`);
-    svg.append('g')
-      .call(yAxis);
+    // Y axis
+    const y = d3.scaleBand()
+      .range([0, height])
+      .domain(data.map(d => d.Country))
+      .padding(.1);
+    svg.append("g")
+      .call(d3.axisLeft(y))
 
-    // setting the svg data
-    svg.selectAll('.bar')
+    //Bars
+    svg.selectAll("myRect")
       .data(data)
-      .join('rect')
-        .attr('x', (v, i) => xScale(i))
-        .attr('y', yScale)
-        .attr('width', xScale.bandwidth())
-        .attr('height', val => height - yScale(val));
+      .join("rect")
+      .attr("x", x(0))
+      .attr("y", d => y(d.Country))
+      .attr("width", d => x(d.Value))
+      .attr("height", y.bandwidth())
+      .attr("fill", "#69b3a2")
 
-  }, [data]);
+  })
 
   return (
     <div className="Bar">
       <svg ref={svgRef}></svg>
     </div>
   );
+
 }
 
-export default Bar;
+// export const Bar = props => {
+//   // const [data] = useState([200, 250, 60, 150, 100, 175]);
+//   const svgRef = useRef();
+//   const { data, width, height } = props;
+
+
+//   useEffect(() => {
+//     // setting up svg container
+//     // const w = 400;
+//     // const h = 300;
+//     const svg = d3.select(svgRef.current)
+//       .attr('width', width)
+//       .attr('height', height)
+//       .style('overflow', 'visibile')
+//       .style('margin-top', '75px');
+
+//     // setting the scaling
+//     const xScale = d3.scaleBand()
+//       .domain(data.map((val, i) => i))
+//       .range([0, width])
+//       .padding(0.5);
+//     const yScale = d3.scaleLinear()
+//       .domain([0, height])
+//       .range([height, 0]);
+
+//     // setting the axis
+//     const xAxis = d3.axisBottom(xScale)
+//       .ticks(data.length);
+//     const yAxis = d3.axisLeft(yScale)
+//       .ticks(5);
+//     svg.append('g')
+//       .call(xAxis)
+//       .attr('transform', `translate(0, ${height})`);
+//     svg.append('g')
+//       .call(yAxis);
+
+//     // setting the svg data
+//     svg.selectAll('.bar')
+//       .data(data)
+//       .join('rect')
+//         .attr('x', (v, i) => xScale(i))
+//         .attr('y', yScale)
+//         .attr('width', xScale.bandwidth())
+//         .attr('height', val => height - yScale(val));
+
+//   }, [data]);
+
+//   return (
+//     <div className="Bar">
+//       <svg ref={svgRef}></svg>
+//     </div>
+//   );
+// }
+
+// export default Bar;
