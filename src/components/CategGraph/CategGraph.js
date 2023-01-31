f62976223db894bf3e68cca9990434ab05906f3a
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-01-31 13:48:35 -0600

    w

--- src/components/CategGraph/CategGraph.js
+++ src/components/CategGraph/CategGraph.js
@@ -7,7 +7,7 @@ import './CategGraph.scss';
 export function CategGraph(){
 
 
-  const [donutData, setData] = useState([{name: "CAD", value: 43}, {name: "USD", value: 412}, {name: "EUR", value: 665}, {name: "GBP", value: 123}])
+  const [donutData, setData] = useState([{name: "CAD", value: 43}, {name: "USD", value: 412}, {name: "EUR", value: 665}, {name: "GBP", value: 123}]);
   const [barData, setBarData] = useState([200, 250, 60, 150, 100, 175]);
 
   
@@ -27,9 +27,9 @@ export function CategGraph(){
       <div>
 
         <Bar
-          data={barData}
-          width={400}
-          height={300}
+          // data={barData}
+          // width={400}
+          // height={300}
         />
 
       </div>
