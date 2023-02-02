9ee08526643c87cfcb784a4b6e6fc481980430a7
BrianKendalBucek <91170533+BrianKendalBucek@users.noreply.github.com>
2023-02-02 17:06:11 -0600

    buttons with Material UI

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
+// import useStyles from '../../styles';
 import Header from "../Header/Header";
 import './Statistics.scss';
 import { Link } from "react-router-dom";
 import { DayChart } from "../Charts/DayChart/DayChart";
 import ProgressBar from "../Charts/ProgressBar/ProgressBar";
+import { Button } from '@mui/material';
 
 //figure out props.completed which would be the percent of budget used
 
 export function Statistics (props){
+
 
   const viewTitle = props.viewTitle;
 
@@ -18,8 +21,6 @@ export function Statistics (props){
 
   const [data, setData] = useState([{ name: "CAD", value: 43 }, { name: "USD", value: 412 }, { name: "EUR", value: 665 }, { name: "GBP", value: 123 }])
 
-  
-  
   return (
 
     <div className="stats-main">
@@ -36,10 +37,13 @@ export function Statistics (props){
         <DayChart />
       </div>
 
-      <div className="stats-btns">
-        <button><Link to="/category"><p>Category</p></Link></button>
-        <button><Link to="/expenses"><p>Expenses</p></Link></button>
-        <button><Link to="/converter"><p>Converter</p></Link></button>
+      <div className="stats-btns view-title">
+        <Button variant="contained" sx={{backgroundColor: '#6D89AE', fontFamily: 'monospace'}}>
+          <Link to="/category"><p>Category</p></Link></Button>
+        <Button variant="contained" sx={{backgroundColor: '#6D89AE', fontFamily: 'monospace'}}>
+          <Link to="/expenses"><p>Expenses</p></Link></Button>
+        <Button variant="contained" sx={{backgroundColor: '#6D89AE', fontFamily: 'monospace'}}>
+          <Link to="/converter"><p>Converter</p></Link></Button>
         {/* 
         Not sure that we will have these here or on their local pages
         <button>Add Expense</button>
