6b5e862c4642ea45041253bdba6fcb92ca51aaaa
TeddyGavi <matc.davis@gmail.com>
2023-02-02 15:57:44 -0800

    working on login with mui

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,24 @@
 import React from "react";
-import './Login.scss';
+import "./Login.scss";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+// import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { useNavigate } from "react-router-dom";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import Header from "../Header/Header";
+import axios from "axios";
 
-export function Login (){
-
-  return (
+export function Login() {
+  /*  return (
     <div className="form">
       <form>
         <div className="input-container">
@@ -19,8 +34,100 @@ export function Login (){
         </div>
       </form>
     </div>
-  );
+  ); */
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData(e.currentTarget);
+    console.log({
+      email: data.get("email"),
+      password: data.get("password"),
+    });
+    const email = data.get("email");
+    const password = data.get("password");
+    const isAuth = await axios.post(
+      "http://localhost:3002/login",
+      { email, password },
+      { withCredentials: true }
+    );
+  };
+
+  return (
+    <>
+      <Header viewTitle={"Login"}></Header>
+      <Container
+        sx={{ fontFamily: "monospace" }}
+        component="main"
+        maxWidth="xs"
+      >
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography
+            sx={{ fontFamily: "monospace" }}
+            component="h1"
+            variant="h5"
+          >
+            Sign in
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{
+                mt: 3,
+                mb: 2,
+                fontFamily: "monospace",
+                bgcolor: "#9ACCE3",
+                "&:hover": { bgcolor: "#6D89AE" },
+              }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item>
+                <Link sx={{ fontFamily: "monospace" }} href="#" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </>
+  );
 }
 
 // reference site
--- src/index.js
+++ src/index.js
@@ -6,30 +6,41 @@ import { Converter } from "./components/Converter/Converter";
 import Error from "./components/Error/Error";
 import { Expenses } from "./components/Expenses/Expenses";
 import { Statistics } from "./components/Statistics/Statistics";
+import { Login } from "./components/Login/Login";
 import Welcome from "./components/Welcome/Welcome";
 
-
-
 export default function App() {
-
   //Sets the apps' main background color
   useLayoutEffect(() => {
-    document.body.style.backgroundColor = "#E5E8ED"
+    document.body.style.backgroundColor = "#E5E8ED";
   });
-  
+
   return (
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Welcome />}></Route>
-        <Route path="/stats" element={<Statistics viewTitle={"Statistics"}/>}></Route>
-        <Route path="/category" element={<CategGraph viewTitle={"Category"}/>}></Route>
-        <Route path="/expenses" element={<Expenses viewTitle={"Expenses"}/>}></Route>
-        <Route path="/converter" element={<Converter viewTitle={"Currency Coverter"}/>}></Route>
+        <Route
+          path="/stats"
+          element={<Statistics viewTitle={"Statistics"} />}
+        ></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route
+          path="/category"
+          element={<CategGraph viewTitle={"Category"} />}
+        ></Route>
+        <Route
+          path="/expenses"
+          element={<Expenses viewTitle={"Expenses"} />}
+        ></Route>
+        <Route
+          path="/converter"
+          element={<Converter viewTitle={"Currency Converter"} />}
+        ></Route>
         <Route path="*" element={<Error />}></Route>
       </Routes>
     </BrowserRouter>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
