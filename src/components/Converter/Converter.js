a33ea7cfacf11061ab54495d7b9171de25851379
Gord L <rndedg@gmail.com>
2023-02-02 13:14:01 -0800

    Add Dropdown menus for selecting Currencies to convert

--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -1,37 +1,79 @@
-import React from 'react';
+import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
+import { Box } from '@mui/system';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import Header from '../Header/Header';
 import './Converter.scss';
 
+
+//useState for map
 export function Converter(props) {
 
+  const [primary, setPrimary] = useState('');
+  const [secondary, setSecondary] = useState('');
+  const [menuCurr, setMenuCurr] = useState([]);
+
+//Need to DRY this up, make it reusable somehow
+
+  const handleChange = (event) => {
+    setPrimary(event.target.value);
+  };
+  const secondaryChange = (event) => {
+    setSecondary(event.target.value)
+  }
   
+  useEffect(()=> {
+      axios.get("http://localhost:3002/api/currency").then((res)=> {
+    const currencyCodes = [];
+    res.data.forEach(ele => {
+    currencyCodes.push({id: ele.id, name: ele.name, code: ele.code, rate: ele.rate_to_usd})
+  })
+    setMenuCurr(currencyCodes)
+  })
+  },[]);
+
+
+
+
   return (
     <div>
       <Header viewTitle={props.viewTitle}/>
-      <h2>Currency Conversion</h2>
-      {/* <div className="primary-amount"> */}
-      {/* show input amount centered */}
-      {/* </div> */}
-      <div>
-        <div className='primary-currency'>
-          {/* show input currency */}
-        </div>
-        <div className='secondary-div'>
-          <div className='secondary-currency'>
-            {/* show secondary currency name left */}
-          </div>
-          <div className='secondary-amount'>
-            {/* show secondary currency amount right */}
-          </div>
-        </div>
-        <div className='primary-amount-input'>
-          <form action="?">
-            <label for="amount">Amount:</label>
-            <input type="integer" id="amount" name="amount"></input>
-            <input type="submit" value="Submit"></input>
-          </form>
-        </div>
-      </div>
+      
+      <Box className="primary-box">
+        <FormControl fullWidth>
+          <InputLabel id="primary-curr-label">Primary</InputLabel>
+          <Select
+            labelId="primary-curr-label"
+            id="primary-curr"
+            label="primary-curr"
+            value={primary}
+            onChange={handleChange}
+          >
+            
+            {menuCurr.map(ele => {
+              return <MenuItem key={ele.id} value={ele.code}>{ele.name}</MenuItem>
+            })}
+          </Select>
+        </FormControl>
+      </Box>
+
+      <Box className="secondary-box">
+        <FormControl fullWidth>
+          <InputLabel id="secondary-curr-label">Secondary</InputLabel>
+          <Select
+            labelId="secondary-curr-label"
+            id="secondary-curr"
+            label="secondary-curr"
+            value={secondary}
+            onChange={secondaryChange}  //This needs to be changed.
+          >
+            
+            {menuCurr.map(ele => {
+              return <MenuItem key={ele.id} value={ele.code}>{ele.name}</MenuItem>
+            })}
+          </Select>
+        </FormControl>
+      </Box>
     </div>
   )
 
