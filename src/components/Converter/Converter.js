0341472c184233d92079985694c55c8e05103a44
Gord L <rndedg@gmail.com>
2023-02-03 15:23:15 -0800

    Added calculcation functionality and most of the styling. Some minor changes could be made but it looks good as is

--- src/components/Converter/Converter.js
+++ src/components/Converter/Converter.js
@@ -1,47 +1,67 @@
-import { FormControl, InputLabel, MenuItem, Select } from '@mui/material';
-import { Box } from '@mui/system';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import Header from '../Header/Header';
-import './Converter.scss';
-
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
+import { Box } from "@mui/system";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Header from "../Header/Header";
+import "./Converter.scss";
 
 //useState for map
 export function Converter(props) {
-
-  const [primary, setPrimary] = useState('');
-  const [secondary, setSecondary] = useState('');
+  const [primary, setPrimary] = useState("");
+  const [secondary, setSecondary] = useState("");
   const [menuCurr, setMenuCurr] = useState([]);
+  const [input, setInput] = useState(0);
+  const [results, setResults] = useState("Results");
 
-//Need to DRY this up, make it reusable somehow
+  //Need to DRY this up, make it reusable somehow
 
   const handleChange = (event) => {
     setPrimary(event.target.value);
   };
   const secondaryChange = (event) => {
-    setSecondary(event.target.value)
-  }
+    setSecondary(event.target.value);
+  };
+
+  useEffect(() => {
+    axios.get("http://localhost:3002/api/currency").then((res) => {
+      const currencyCodes = [];
+      res.data.forEach((ele) => {
+        currencyCodes.push({
+          id: ele.id,
+          name: ele.name,
+          code: ele.code,
+          rate: ele.rate_to_usd,
+        });
+      });
+      setMenuCurr(currencyCodes);
+    });
+  }, []);
+
   
-  useEffect(()=> {
-      axios.get("http://localhost:3002/api/currency").then((res)=> {
-    const currencyCodes = [];
-    res.data.forEach(ele => {
-    currencyCodes.push({id: ele.id, name: ele.name, code: ele.code, rate: ele.rate_to_usd})
-  })
-    setMenuCurr(currencyCodes)
-  })
-  },[]);
+  const calculate = () => {
+
+    const code = menuCurr.find(x => x.code === primary).name
+    const primaryRate = menuCurr.find(x => x.code === primary).rate
+    const secondaryRate = menuCurr.find(x => x.code === secondary).rate
+    const calcRate = (secondaryRate * (1 / primaryRate))
 
 
+    return (setResults((input * calcRate).toFixed(2) + " " +  code));
+  }
 
 
   return (
     <div>
-      <Header viewTitle={props.viewTitle}/>
-      
+      <Header viewTitle={props.viewTitle} />
+
+      <div className="results">
+        <h2>{results}</h2>
+      </div>
+
+
       <Box className="primary-box">
         <FormControl fullWidth>
-          <InputLabel id="primary-curr-label">Primary</InputLabel>
+          <InputLabel id="primary-curr-label">Primary Currency</InputLabel>
           <Select
             labelId="primary-curr-label"
             id="primary-curr"
@@ -49,32 +69,55 @@ export function Converter(props) {
             value={primary}
             onChange={handleChange}
           >
-            
-            {menuCurr.map(ele => {
-              return <MenuItem key={ele.id} value={ele.code}>{ele.name}</MenuItem>
+            {menuCurr.map((ele) => {
+              return (
+                <MenuItem key={ele.id} value={ele.code}>
+                  {ele.code.toUpperCase()} - {ele.name}
+                </MenuItem>
+              );
             })}
           </Select>
         </FormControl>
       </Box>
 
+
       <Box className="secondary-box">
         <FormControl fullWidth>
-          <InputLabel id="secondary-curr-label">Secondary</InputLabel>
+          <InputLabel id="secondary-curr-label">Secondary Currency</InputLabel>
           <Select
             labelId="secondary-curr-label"
             id="secondary-curr"
             label="secondary-curr"
             value={secondary}
-            onChange={secondaryChange}  //This needs to be changed.
+            onChange={secondaryChange} //This needs to be changed.
           >
-            
-            {menuCurr.map(ele => {
-              return <MenuItem key={ele.id} value={ele.code}>{ele.name}</MenuItem>
+            {menuCurr.map((ele) => {
+              return (
+                <MenuItem key={ele.id} value={ele.code}>
+                  {ele.code.toUpperCase()} - {ele.name}
+                </MenuItem>
+              );
             })}
           </Select>
         </FormControl>
       </Box>
-    </div>
-  )
 
+      <TextField id="standard-basic" label="Enter value" variant="standard" className="value" 
+      onChange={
+        (e) => {
+          setInput(e.target.value);
+        }
+        }/>
+
+      <br/>
+
+      <Button variant="contained" id="calc-submit"
+              onClick={() => {
+                calculate();
+      }}
+      >
+        Submit
+      </Button>
+    </div>
+  );
 }
