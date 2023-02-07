860f9cba9032fe222017ed16dd011b9e63e84680
TeddyGavi <matc.davis@gmail.com>
2023-02-06 16:04:10 -0800

    setting up delete modal

--- src/components/CategoryList/CategoryList.js
+++ src/components/CategoryList/CategoryList.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Header from "../Header/Header";
 import axios from "axios";
 import "./CategoryList.scss";
@@ -16,11 +16,33 @@ import {
   Container,
   Divider,
   Button,
+  Modal,
 } from "@mui/material";
+import { CategorySharp, DeleteOutlineOutlined } from "@mui/icons-material";
+
+const modalStyle = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 200,
+  bgcolor: "#E5E8ED",
+  border: "1px solid #9ACCE3",
+  boxShadow: 24,
+  borderRadius: "10px",
+  p: 4,
+  fontFamily: "monospace",
+};
 
 export function CategoryList(props) {
   const [category, setCategory] = useState([]);
-  const [newCategory, setNewCategory] = useState("");
+  const [open, setOpen] = useState(false);
+  const [toDelete, setToDelete] = useState(0);
+  const handleOpen = (id) => {
+    setToDelete(id);
+    setOpen(true);
+  };
+  const handleClose = () => setOpen(false);
 
   const getCategories = () => {
     return axios
@@ -36,27 +58,35 @@ export function CategoryList(props) {
     getCategories();
   }, []);
 
-  const categories = category.map((category) => {
-    return (
-      <li className="category-name" key={category.id}>
-        {category.name}
-      </li>
-    );
-  });
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    return axios
+    const data = new FormData(e.currentTarget);
+    const category = data.get("category");
+    axios
       .post(
         "http://localhost:3002/api/categories",
         {
-          categoryName: newCategory,
+          categoryName: category,
         },
         { withCredentials: true }
       )
       .then(() => {
         getCategories();
       });
+  };
+
+  const handleDelete = () => {
+    axios({
+      method: "DELETE",
+      url: "http://localhost:3002/api/categories/delete",
+      data: { catId: toDelete },
+      withCredentials: true,
+    })
+      .then(() => {
+        setOpen(false);
+        getCategories();
+      })
+      .catch((err) => console.log(err));
   };
 
   return (
@@ -67,6 +97,38 @@ export function CategoryList(props) {
         component="main"
         maxWidth="xs"
       >
+        <div>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box sx={modalStyle}>
+              <Typography id="modal-modal-title" variant="h6" component="h2">
+                Are you sure?
+              </Typography>
+              <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                This action cannot be reversed.
+              </Typography>
+              <Button
+                onClick={() => handleDelete()}
+                type="submit"
+                variant="contained"
+                fullWidth
+                sx={{
+                  mt: 3,
+                  mb: 2,
+                  fontFamily: "monospace",
+                  bgcolor: "#9ACCE3",
+                  "&:hover": { bgcolor: "red" },
+                }}
+              >
+                Confirm
+              </Button>
+            </Box>
+          </Modal>
+        </div>
         <Grid item xs={12} md={6}>
           <Typography
             sx={{ mt: 4, mb: 2, fontFamily: "Orbitron" }}
@@ -74,29 +136,40 @@ export function CategoryList(props) {
             component="div"
           ></Typography>
           <List>
-            {category.map((cat) => (
-              <ListItem
-                key={cat.id}
-                secondaryAction={
-                  <IconButton edge="end" aria-label="delete"></IconButton>
-                }
-              >
-                <ListItemAvatar>
-                  <Avatar></Avatar>
-                </ListItemAvatar>
-                <ListItemText primary={cat.name}></ListItemText>
-              </ListItem>
+            {category.map((cat, i) => (
+              <>
+                <ListItem
+                  key={cat.id}
+                  secondaryAction={
+                    <IconButton
+                      edge="end"
+                      aria-label="delete"
+                      onClick={() => handleOpen(cat.id)}
+                    >
+                      <DeleteOutlineOutlined sx={{ bgcolor: "E5E8ED" }} />
+                    </IconButton>
+                  }
+                >
+                  <ListItemAvatar>
+                    <Avatar>
+                      <CategorySharp />
+                    </Avatar>
+                  </ListItemAvatar>
+                  <ListItemText primary={cat.name}></ListItemText>
+                </ListItem>
+                <Divider />
+              </>
             ))}
           </List>
         </Grid>
 
-        <Typography sx={{ fontFamily: "monospace" }}>Add New</Typography>
+        <Typography sx={{ mt: 4, fontFamily: "monospace" }}>Add New</Typography>
         <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
           <TextField
             margin="normal"
             required
             fullWidth
-            id="email"
+            id="category"
             label="Category"
             name="category"
             autoFocus
@@ -104,6 +177,7 @@ export function CategoryList(props) {
           <Button
             type="submit"
             variant="contained"
+            fullWidth
             sx={{
               mt: 3,
               mb: 2,
@@ -112,7 +186,7 @@ export function CategoryList(props) {
               "&:hover": { bgcolor: "#9ACCE3" },
             }}
           >
-            +
+            Add
           </Button>
         </Box>
       </Container>
